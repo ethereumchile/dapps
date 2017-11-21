a0a237056f8c35b461bcc2eaef355596a8b8d653
ethereumchile <33845595+ethereumchile@users.noreply.github.com>
2017-11-21 02:10:32 -0300

    Publicación del archivo dMessenger v1.sol
    dMessenger es un Contrato Inteligente que funciona en la Blockchain de Ethereum y su única función es recibir mensaje y almacenarlos para ser mostrados posteriormente de forma pública en el sitio web https://dapps.ethereumchile.cl o por cualquier desarrollador con un poco de conocimiento de programación en Javascript u otro lenguaje que permita la interacción con la Blockchain Ethereum.
    
    Puede ser considerado tanto un chat como un libro de visitas descentralizado, público o privado, según con quién se comparta la dirección del contrato inteligente.

---
+++ dMessenger/dMessneger v1.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.4.18;
+
+contract dMessenger {
+	
+	address public creador;
+	uint public contadorMensajes = 0;
+
+	struct Mensaje {
+
+		address emisor;
+		string mensaje;
+		uint fechaPublicacion;
+
+	}
+
+	mapping(uint => Mensaje) public mensajes;
+
+	function dMessenger () public payable {
+
+		creador = msg.sender;
+
+	}
+
+	function escribirMensaje(string mensaje) public {
+
+		mensajes[contadorMensajes].emisor = msg.sender;
+		mensajes[contadorMensajes].mensaje = mensaje;
+		mensajes[contadorMensajes].fechaPublicacion = block.timestamp;
+		contadorMensajes++;
+
+	}
+
+}
